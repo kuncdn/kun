3242429a2eb3b19f455c36261387c915b530aefd
aapeli <aapeli@aapeli.nian@gmail.com>
2019-10-02 22:37:32 +0800

    Change License.

--- cmd/tracfox/app/options/options.go
+++ cmd/tracfox/app/options/options.go
@@ -1,7 +1,5 @@
-package options
-
 /*
-Copyright 2019 The Labchan Authors.
+Copyright 2019 The Tracfox Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -12,6 +10,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+package options
 
 import (
 	"errors"
